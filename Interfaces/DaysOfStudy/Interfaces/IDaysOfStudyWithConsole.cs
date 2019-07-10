eede4824d5a4620abcbacf6c05dae1a70c137029
dimaematveev <dimaematveev@gmail.com>
2019-07-11 00:44:32 +0300

    Создан пустой проект DaysOfStudy.

---
+++ Interfaces/DaysOfStudy/Interfaces/IDaysOfStudyWithConsole.cs
@@ -0,0 +1,14 @@
+﻿using Interface.Interface;
+using Interface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semester.Interfaces
+{
+    public interface IDaysOfStudyWithConsole : IDaysOfStudy, IConsole
+    {
+    }
+}
