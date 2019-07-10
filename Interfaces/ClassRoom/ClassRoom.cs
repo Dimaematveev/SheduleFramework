e12839dba44a46735621fa58ee64995c30a8c0dc
dimaematveev <dimaematveev@gmail.com>
2019-07-10 23:02:06 +0300

    Простейший вариант Аудиторий готов!!!

---
+++ Interfaces/ClassRoom/ClassRoom.cs
@@ -0,0 +1,29 @@
+﻿using ClassRoom.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom : IClassRoomWithConsole
+    {
+        
+
+        public string NameClass { get ; set ; }
+        public int NumberOfPeople { get; set; }
+
+        public ClassRoom(string nameClass, int numberOfPeople)
+        {
+            //TODO:Проверка что Number>0
+            NameClass = nameClass;
+            NumberOfPeople = numberOfPeople;
+        }
+
+        public void ToConsole()
+        {
+            Console.WriteLine($"Аудитория {NameClass} вмещает {NumberOfPeople}.");
+        }
+    }
+}
--- Interfaces/ClassRoom/Program.cs
+++ Interfaces/ClassRoom/Program.cs
@@ -10,6 +10,18 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            List<ClassRoom> classRooms = new List<ClassRoom>();
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                classRooms.Add(new ClassRoom(Guid.NewGuid().ToString().Substring(0, 6), rnd.Next(15, 120)));
+            }
+            foreach (var item in classRooms)
+            {
+                item.ToConsole();
+            }
+
+            Console.ReadLine();
         }
     }
 }
