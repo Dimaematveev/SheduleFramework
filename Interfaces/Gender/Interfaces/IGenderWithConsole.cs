99307362b92835b3b55a1f96811d8f1e45c190a3
dimaematveev <dimaematveev@gmail.com>
2019-07-11 00:05:27 +0300

    Пустой проект гендера.

---
+++ Interfaces/Gender/Interfaces/IGenderWithConsole.cs
@@ -0,0 +1,13 @@
+﻿using Interface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gender.Interfaces
+{
+    interface IGenderWithConsole:IGender,IConsole
+    {
+    }
+}
