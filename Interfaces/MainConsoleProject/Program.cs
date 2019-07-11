989e4cf4340e8cd131d51e79a145e50b6728bf8b
dimaematveev <dimaematveev@gmail.com>
2019-07-11 20:47:35 +0300

    Минимум в Main.

--- Interfaces/MainConsoleProject/Program.cs
+++ Interfaces/MainConsoleProject/Program.cs
@@ -10,21 +10,85 @@ namespace MainConsoleProject
     {
         static void Main(string[] args)
         {
-            
+            Gender.Gender gender = new Gender.Gender("мужской");
+            var genders = new List<Gender.Gender>
+            {
+                new Gender.Gender("мужской"),
+                new Gender.Gender("женский"),
+            };
             ClassRoom.ClassRoom classRoom = new ClassRoom.ClassRoom("Аудит1", 101);
-            DaysOfStudy.DaysOfStudy daysOfStudy;
-            Gender.Gender gender = new Gender.Gender("Мужчина");
+            var classRooms = new List<ClassRoom.ClassRoom>
+            {
+                new ClassRoom.ClassRoom("Аудит1", 50),
+                new ClassRoom.ClassRoom("Аудит2", 34),
+                new ClassRoom.ClassRoom("Аудит3", 45),
+                new ClassRoom.ClassRoom("Аудит4", 107),
+                new ClassRoom.ClassRoom("Аудит5", 31),
+            };
             Group.Group group = new Group.Group("TBO", 35, 2, "TBO-2", Interface.Interface.TypeStudy.FullTimeEducation);
-            Person.Person person = new Person.Person("Dima", gender, new DateTime(1996, 05, 19), "Юго западная");
+            var groups = new List<Group.Group>
+            {
+                new Group.Group("TBO", 35, 1, "TBO-1", Interface.Interface.TypeStudy.FullTimeEducation),
+                new Group.Group("TBO", 23, 2, "TBO-2", Interface.Interface.TypeStudy.FullTimeEducation),
+                new Group.Group("TMO", 11, 3, "TMO-3", Interface.Interface.TypeStudy.FullTimeEducation),
+                new Group.Group("TCO", 54, 2, "TCO-2", Interface.Interface.TypeStudy.FullTimeEducation),
+            };
             Subject.Subject subject = new Subject.Subject("Математика");
+            var subjects = new List<Subject.Subject>
+            {
+                 new Subject.Subject("программирование 2 курс"),
+                 new Subject.Subject("математический анализ 3 курс"),
+                 new Subject.Subject("введение в специальность 1 курс"),
+                 new Subject.Subject("история России 1-4 курс"),
+            };
+            //Явная недоработка Надо добавить еще один класс как Subject+int и сюда добавить List этого класса
             PlanOfLessons.PlanOfLessons planOfLessons = new PlanOfLessons.PlanOfLessons(group, subject, 14);
-            Semester.Semester semester = new Semester.Semester(new DateTime(2019, 02, 02), new DateTime(2019, 06, 01));
-            Student.Student student = new Student.Student(group, person);
+            Semester.Semester semester = new Semester.Semester(new DateTime(2019, 02, 02), new DateTime(2019, 05, 31));
+           
+
+            DaysOfStudy.DaysOfStudy daysOfStudy = new DaysOfStudy.DaysOfStudy(DateTime.Now,Interface.Interface.HowDays.DayOff);
+            Person.Person person = new Person.Person("Dima", gender, new DateTime(1996, 05, 19), "Юго западная");
+            Student.Student student = new Student.Student(group, person);//Пока бессмысленно делать.
             SubjectOfTeacher.SubjectOfTeacher subjectOfTeacher = new SubjectOfTeacher.SubjectOfTeacher(subject, 10);
             Teacher.Teacher teacher = new Teacher.Teacher(new List<SubjectOfTeacher.Interfaces.ISubjectOfTeacherWithConsole> { subjectOfTeacher }, "none", 1, person);
-            TimeLessons.TimeLessons timeLessons = new TimeLessons.TimeLessons(new TimeSpan(09, 00, 00), new TimeSpan(10, 30, 00), 1);
-            TypeLessons.TypeLessons typeLessons = new TypeLessons.TypeLessons("Лекция");
+            var teachers = new List<Teacher.Teacher>();
+            string[] names = { "Александр", "Максим", "Виктор", "Дмитрий", "Олег" };
+            for (int i = 0; i < 5; i++)
+            {
+                
+                int r1 = i * 5 % subjects.Count;
+                int r2 = i * 3 % subjects.Count;
+                if (r1==r2)
+                {
+                    r1=(r1+1)% subjects.Count;
+                }
+                var subjectOfTeacher1 = new List<SubjectOfTeacher.Interfaces.ISubjectOfTeacherWithConsole>
+                {
+                    new SubjectOfTeacher.SubjectOfTeacher(subjects[r1], 100),
+                    new SubjectOfTeacher.SubjectOfTeacher(subjects[r2], 100),
+                };
+                var person1 = new Person.Person(names[i],genders[0],new DateTime(1958+i,(17*i)%12+1,(23*i)%28+1),"Not info");
+                var teacher1 = new Teacher.Teacher(subjectOfTeacher1,"none",1, person1);
+                teachers.Add(teacher1);
+            }
 
+            TimeLessons.TimeLessons timeLessons = new TimeLessons.TimeLessons(new TimeSpan(09, 00, 00), new TimeSpan(10, 30, 00), 1);
+            var timeLessonss = new List<TimeLessons.TimeLessons>
+            {
+                new TimeLessons.TimeLessons(new TimeSpan(09, 00, 00), new TimeSpan(10, 30, 00), 1),
+                new TimeLessons.TimeLessons(new TimeSpan(10, 40, 00), new TimeSpan(12, 10, 00), 2),
+                new TimeLessons.TimeLessons(new TimeSpan(13, 00, 00), new TimeSpan(14, 30, 00), 3),
+                new TimeLessons.TimeLessons(new TimeSpan(14, 40, 00), new TimeSpan(16, 10, 00), 4),
+                new TimeLessons.TimeLessons(new TimeSpan(16, 20, 00), new TimeSpan(17, 50, 00), 5),
+                new TimeLessons.TimeLessons(new TimeSpan(18, 00, 00), new TimeSpan(19, 30, 00), 6),
+            };
+            TypeLessons.TypeLessons typeLessons = new TypeLessons.TypeLessons("Лекция");
+            var typeLessonss = new List<TypeLessons.TypeLessons>
+            {
+                new TypeLessons.TypeLessons("Лекция"),
+                new TypeLessons.TypeLessons("Практика"),
+            };
+            Console.ReadLine();
         }
     }
 }
