cdb2ba6cfda8887fe34a5d07f0d14aba14d9dda1
dimaematveev <dimaematveev@gmail.com>
2019-07-14 16:03:04 +0300

    Время пары исключения!

--- Interfaces/TimeLessons/TimeLessons.cs
+++ Interfaces/TimeLessons/TimeLessons.cs
@@ -16,6 +16,26 @@ namespace TimeLessons
 
         public TimeLessons(TimeSpan beginTime, TimeSpan endTime, int numberLessons)
         {
+            if (beginTime==null)
+            {
+                throw new ArgumentNullException("Время начала пары не должно быть пустым!", nameof(beginTime));
+            }
+            if (endTime == null)
+            {
+                throw new ArgumentNullException("Время окончания пары не должно быть пустым!", nameof(endTime));
+            }
+            if (numberLessons <=0)
+            {
+                throw new ArgumentException("Номер пары не должен быть меньше 0!", nameof(numberLessons));
+            }
+            if (beginTime  >= endTime)
+            {
+                throw new ArgumentException("Время начала пары должно быть меньше времени окончания пары!", nameof(beginTime) +" "+ nameof(endTime));
+            }
+            if ((endTime - beginTime).Hours > 12) 
+            {
+                throw new ArgumentException("Пара не может быть больше 12 часов!", nameof(beginTime) + " " + nameof(endTime));
+            }
             BeginTime = beginTime;
             EndTime = endTime;
             NumberLessons = numberLessons;
