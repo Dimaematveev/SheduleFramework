4a49fe521dc6ea8fe3034417a496843c1f41adb6
dimaematveev <dimaematveev@gmail.com>
2019-07-14 15:52:58 +0300

    Предмет для учителя Исключения!

--- Interfaces/SubjectOfTeacher/SubjectOfTeacher.cs
+++ Interfaces/SubjectOfTeacher/SubjectOfTeacher.cs
@@ -14,6 +14,18 @@ namespace SubjectOfTeacher
         public int Percent { get; set; }
         public SubjectOfTeacher(ISubject subject, int percent)
         {
+            if (subject == null)
+            {
+                throw new ArgumentNullException("Предмет не должен быть пустым!", nameof(subject));
+            }
+            if (percent <0)
+            {
+                throw new ArgumentNullException("Процент не должен быть меньше 0!", nameof(percent));
+            }
+            if (percent > 100) 
+            {
+                throw new ArgumentNullException("Процент не должен быть больше 100!", nameof(percent));
+            }
             Subject = subject;
             Percent = percent;
         }
