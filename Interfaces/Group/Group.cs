bbfea9551ce228777f9eaf884c2d6778ef32a0e0
dimaematveev <dimaematveev@gmail.com>
2019-07-17 18:15:47 +0300

    Группы и студенты сделал.

--- Interfaces/Group/Group.cs
+++ Interfaces/Group/Group.cs
@@ -9,20 +9,32 @@ using System.Threading.Tasks;
 
 namespace Group
 {
+    /// <summary>
+    /// Класс Группа студентов
+    /// </summary>
     public class Group : IGroupWithConsole
     {
-        
 
+        /// <value>Имя группы.</value>
         public string NameGroup { get; set; }
-        public List<IStudent> Students { get; set; }
+        /// <value>Количество человек в группе(Берется из списка студентов).</value>
         public int NumberOfStutents { get => Students.Count; }
+        /// <value>Курс.</value>
         public int Cours { get; set; }
+        /// <value>Семинар.</value>
         public string Seminar { get; set; }
+        /// <value>Тип обучения.</value>
         public TypeStudy TypeOfTraining { get; set; }
-        
-
+        /// <value>Список студентов.</value>
+        public List<IStudent> Students { get; set; } 
+        /// <summary>
+        /// Конструктор класса группы. С пустым списком студентов.
+        /// </summary>
+        /// <param name="nameGroup">Имя группы.</param>
+        /// <param name="cours">Курс.</param>
+        /// <param name="seminar">Семинар.</param>
+        /// <param name="typeOfTraining">Тип обучения.</param>
         public Group(   string nameGroup,
-                        List<IStudent> students, 
                         int cours, 
                         string seminar, 
                         TypeStudy typeOfTraining)
@@ -31,10 +43,6 @@ namespace Group
             if (string.IsNullOrWhiteSpace(nameGroup))
             {
                 throw new ArgumentNullException("Имя группы не должно быть пустым!", nameof(nameGroup));
-            }
-            if (students.Count<= 0 || students == null)
-            {
-                throw new ArgumentNullException($"Количество студентов в группе должно быть больше нуля!",nameof(students));
             }
             if (cours < 1 || cours>5)
             {
@@ -45,11 +53,73 @@ namespace Group
                 throw new ArgumentNullException("Семинар не должен быть пустым.", nameof(seminar));
             }
             NameGroup = nameGroup;
-            Students = students;
             Cours = cours;
             Seminar = seminar;
             TypeOfTraining = typeOfTraining;
+            Students = new List<IStudent>();
         }
+        /// <summary>
+        /// Конструктор группы со списком студентов. Автоматически вызывает другой конструктор. И добавляет студентов.
+        /// </summary>
+        /// <param name="nameGroup">Имя группы.</param>
+        /// <param name="cours">Курс.</param>
+        /// <param name="seminar">Семинар.</param>
+        /// <param name="typeOfTraining">Тип обучения.</param>
+        /// <param name="students">Список студентов.</param>
+        public Group(string nameGroup,
+                        int cours,
+                        string seminar,
+                        TypeStudy typeOfTraining,
+                        List<IStudent> students) : this(nameGroup, cours, seminar, typeOfTraining)
+        {
+            AddStudent(students);
+        }
+        /// <summary>
+        /// Добавление студентов в группу(одного).
+        /// </summary>
+        /// <param name="student">Студент.</param>
+        public bool AddStudent(IStudent student)
+        {
+            if (student == null)
+            {
+                throw new ArgumentNullException($"Количество студентов в группе должно быть больше нуля!", nameof(student));
+            }
+            if (Equals(student.Group,this) || Equals(student.Group, null))
+            {
+                student.Group = this;
+                Students.Add(student);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        /// <summary>
+        /// Добавление студентов в группу(Список).
+        /// </summary>
+        /// <param name="students">Список студентов.</param>
+        public bool AddStudent(List<IStudent> students)
+        {
+            if (students.Count <= 0 || students == null)
+            {
+                throw new ArgumentNullException($"Количество студентов в группе должно быть больше нуля!", nameof(students));
+            }
+            if (students.All(student=>Equals(student.Group, this)|| Equals(student.Group, null)))
+            {
+                students.ForEach(stud => stud.Group = this);
+                Students.AddRange(students);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Вывод на консоль.
+        /// </summary>
         public void ToConsole()
         {
             Console.WriteLine(Seminar);
--- Interfaces/Student/Student.cs
+++ Interfaces/Student/Student.cs
@@ -9,24 +9,59 @@ using System.Threading.Tasks;
 
 namespace Student
 {
+    /// <summary>
+    /// Класс студент.
+    /// </summary>
     public class Student : IStudentWithConsole
     {
+        /// <value> Группа студента. </value>
         public IGroup Group { get ; set; }
+        /// <value> Человек(стандартный набор).</value>
         public IPerson Person { get ; set; }
-
-        public Student(IGroup group, IPerson person)
+        /// <summary>
+        /// Конструктор студента. С пустой группой.
+        /// </summary>
+        /// <param name="person">Человек.</param>
+        public Student(IPerson person)
+        {
+            if (person == null)
+            {
+                throw new ArgumentNullException("Человек не должен быть пуст!", nameof(person));
+            }
+            Person = person;
+            Group = null;
+        }
+        /// <summary>
+        /// Конструктор студента.Вызывает другой конструктор.
+        /// </summary>
+        /// <param name="person">Человек.</param>
+        /// <param name="group">Группа.</param>
+        public Student(IPerson person, IGroup group):this(person)
+        {
+            EditOrNewGroup(group);
+        }
+        /// <summary>
+        /// Удаляет группу у студента.
+        /// </summary>
+        public void DelGroup()
+        {
+            Group = null;
+        }
+        /// <summary>
+        /// Добавляет или изменяет группу у студента.
+        /// </summary>
+        /// <param name="group">Группа.</param>
+        public void EditOrNewGroup(IGroup group)
         {
             if (group == null)
             {
                 throw new ArgumentNullException("Группа не должна быть пустой!", nameof(group));
             }
-            if (person == null)
-            {
-                throw new ArgumentNullException("Человек не должен быть пуст!", nameof(person));
-            }
             Group = group;
-            Person = person;
         }
+        /// <summary>
+        /// Вывод на консоль.
+        /// </summary>
         public void ToConsole()
         {
             ((IConsole)Group).ToConsole();
