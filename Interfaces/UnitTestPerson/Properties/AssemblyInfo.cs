91f560e3d3d19467e88cdf64177ccd95ba16db0f
dimaematveev <dimaematveev@gmail.com>
2019-07-22 10:55:06 +0300

    Начал делать тест для человека.

---
+++ Interfaces/UnitTestPerson/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPerson")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPerson")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5669ec9a-8663-46b3-8e4a-a9b72d118950")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Interfaces/UnitTestPerson/UnitTestPerson.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Interface.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPerson
+{
+    [TestClass]
+    public class UnitTestPerson
+    {
+        /// <value> стандартные значения и классы с которыми программа БУДЕТ работать  </value>
+        string standatrName;
+        IGender standatrGender;
+        DateTime standatrBirthDay;
+        int standatrAge;
+        string standatrLiving;
+        private void ResetStandart()
+        {
+            standatrName = "Name1";
+            standatrGender = new Gender.Gender("men");
+            standatrAge = 19;
+            standatrBirthDay = new DateTime(DateTime.Now.Year- standatrAge, DateTime.Now.Month, DateTime.Now.Day);
+            standatrLiving = "unknown";
+        }
+        /// <summary>
+        /// Проверка что стандартные параметры делаю конструктор без исключения.
+        /// </summary>
+        [TestMethod]
+        public void TestStandartValue()
+        {
+            ResetStandart();
+            //arrange
+            //act
+            var person = new Person.Person(standatrName, standatrGender, standatrBirthDay, standatrLiving);
+            //assert
+            Assert.AreEqual(standatrName, person.Name);
+            Assert.AreEqual(standatrGender, person.Gender);
+            Assert.AreEqual(standatrBirthDay, person.BirthDay);
+            Assert.AreEqual(standatrAge, person.Age);
+            Assert.AreEqual(standatrLiving, person.Living);
+        }
+    }
+}
