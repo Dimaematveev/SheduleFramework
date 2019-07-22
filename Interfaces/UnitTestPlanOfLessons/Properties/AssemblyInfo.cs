a10484b76379a3b6b6c49a2e3d6e4e1f17f97865
dimaematveev <dimaematveev@gmail.com>
2019-07-22 14:57:42 +0300

    Создал пустойТест для tPlanOfLessons

---
+++ Interfaces/UnitTestPlanOfLessons/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPlanOfLessons")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPlanOfLessons")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c90065cd-fb14-4a63-a823-94794df83147")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Interfaces/UnitTestPlanOfLessons/UnitTestPlanOfLessons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPlanOfLessons
+{
+    [TestClass]
+    public class UnitTestPlanOfLessons
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
