9ef4b7832323cc284562fe90c76409fc35e76942
dimaematveev <dimaematveev@gmail.com>
2019-07-25 22:14:14 +0300

    Тест для человека не надо изменять теперь!

--- Interfaces/UnitTestPerson/UnitTestPerson.cs
+++ Interfaces/UnitTestPerson/UnitTestPerson.cs
@@ -110,21 +110,66 @@ namespace UnitTestPerson
         /// <summary>
         /// Проверка свойства Age.  Надо часто обновлять!!!!!!
         /// </summary>
-        /// <param name="stingBirthDay">Строка даты. Должна быть нормальным.</param>
+        /// <param name="stingBirthDay">Строка даты. Должна быть нормальным. либо (now+/-x) x-количество от сегодня</param>
         /// <param name="age">Возраст.</param>
         [DataTestMethod]
-        [DataRow("11.01.1969",50)]
-        [DataRow("11.12.1969",49)]
-        [DataRow("22.07.1969",50)]
-        [DataRow("23.07.1969",49)]
-        [DataRow("11.01.2018", 1)]
+        [DataRow("now.now-1.now-50", 50)]
+        [DataRow("now-1.now.now-50", 50)]
+        [DataRow("now.now.now-50", 50)]
+        [DataRow("now+1.now.now-50", 49)]
+        [DataRow("now.now+1.now-50", 49)]
 
         public void TestPropertyAge_True(string stingBirthDay, int age)
         {
             //arrange
             ResetStandart();
+            DateTime birthDay;
+            if (stingBirthDay.ToLower().Contains("now"))
+            {
+                int[] date = new int[3]
+                {
+                    DateTime.Now.Day,
+                    DateTime.Now.Month,
+                    DateTime.Now.Year
+                };
+                string[] str = stingBirthDay.ToLower().Split('.');
+                for (int i = 0; i < 3; i++)
+                {
+
+                    if (str[i].Contains("now"))
+                    {
+                        if (str[i].Contains("-"))
+                        {
+                            string[] a = str[i].Split('-');
+                            date[i] -= Convert.ToInt32(a[1]);
+                        }
+                        else if (str[i].Contains("+"))
+                        {
+                            string[] a = str[i].Split('+');
+                            date[i] += Convert.ToInt32(a[1]);
+                        }
+                        else if (str[i].Equals("now"))
+                        {
+
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+
+                    }
+                    else
+                    {
+                        date[i] = Convert.ToInt32(str[i]);
+                    }
+                }
+                birthDay = new DateTime(date[2], date[1], date[0]);
+            }
+            else
+            {
+                birthDay = DateTime.Parse(stingBirthDay);
+            }
             
-            DateTime birthDay = DateTime.Parse(stingBirthDay);
 
             //act
             var person = new Person.Person(standatrName,standatrGender,  birthDay, standatrLiving);
@@ -141,17 +186,62 @@ namespace UnitTestPerson
         /// <param name="living">Где живет.</param>
         /// <param name="toSting">Какая строка должна получиться.</param>
         [DataTestMethod]
-        [DataRow("Name1", "men", "11.01.1969", "HZ", "Пол men имя Name1 возраст 50")]
-        [DataRow("Name2", "women", "11.12.1969", "HZ", "Пол women имя Name2 возраст 49")]
-        [DataRow("Name3", "men", "22.07.1969", "HZ", "Пол men имя Name3 возраст 50")]
-        [DataRow("Name4", "men", "23.07.1969", "HZ", "Пол men имя Name4 возраст 49")]
-        [DataRow("Name5", "woemen", "11.01.2018", "HZ",  "Пол woemen имя Name5 возраст 1")]
+        [DataRow("Name1", "men", "now.now-1.now-50", "HZ", "Пол men имя Name1 возраст 50")]
+        [DataRow("Name2", "women", "now-1.now.now-50", "HZ", "Пол women имя Name2 возраст 50")]
+        [DataRow("Name3", "men", "now.now.now-50", "HZ", "Пол men имя Name3 возраст 50")]
+        [DataRow("Name4", "men", "now+1.now.now-50", "HZ", "Пол men имя Name4 возраст 49")]
+        [DataRow("Name5", "woemen", "now.now+1.now-50", "HZ",  "Пол woemen имя Name5 возраст 49")]
         public void TestToString_True(string name, string stingGender, string stingBirthDay, string living, string toSting)
         {
             //arrange
             ResetStandart();
             IGender gender = new Gender.Gender(stingGender);
-            DateTime birthDay = DateTime.Parse(stingBirthDay);
+            DateTime birthDay;
+            if (stingBirthDay.ToLower().Contains("now"))
+            {
+                int[] date = new int[3]
+                {
+                    DateTime.Now.Day,
+                    DateTime.Now.Month,
+                    DateTime.Now.Year
+                };
+                string[] str = stingBirthDay.ToLower().Split('.');
+                for (int i = 0; i < 3; i++)
+                {
+
+                    if (str[i].Contains("now"))
+                    {
+                        if (str[i].Contains("-"))
+                        {
+                            string[] a = str[i].Split('-');
+                            date[i] -= Convert.ToInt32(a[1]);
+                        }
+                        else if (str[i].Contains("+"))
+                        {
+                            string[] a = str[i].Split('+');
+                            date[i] += Convert.ToInt32(a[1]);
+                        }
+                        else if (str[i].Equals("now"))
+                        {
+
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+
+                    }
+                    else
+                    {
+                        date[i] = Convert.ToInt32(str[i]);
+                    }
+                }
+                birthDay = new DateTime(date[2], date[1], date[0]);
+            }
+            else
+            {
+                birthDay = DateTime.Parse(stingBirthDay);
+            }
             //act
             var person = new Person.Person(name, gender, birthDay, living);
             //assert
