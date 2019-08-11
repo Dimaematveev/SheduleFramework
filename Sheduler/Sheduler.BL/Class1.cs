ded5f7825b68e51142294afb1938e6ba519605a9
dimaematveev <dimaematveev@gmail.com>
2019-08-12 00:23:54 +0300

    Изменил Место сборки!

---
+++ Sheduler/Sheduler.BL/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Interface.Interface;
+using Interface.Interfaces;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+
+namespace Sheduler.BL
+{
+    public class Class1
+    {
+        ISemester semester = A.Fake<ISemester>();
+       
+    }
+}
