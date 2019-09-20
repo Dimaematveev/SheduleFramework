f0c3a3955a7467cf879108b15718fae5cbe3f2d5
dimaematveev <dimaematveev@gmail.com>
2019-09-20 21:36:05 +0300

    Добавили Конфиг для Подключения

---
+++ SimpleSheduler/SimpleSheduler.BD/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSheduler.BD
+{
+    /// <summary>
+    /// Контекст базы данных - лучше приватный
+    /// </summary>
+    public class MyDbContext : DbContext
+    {
+        /// <summary>
+        /// Передаем будующую строку подключения
+        /// </summary>
+        protected MyDbContext() : base("DbConnectionString")
+        {
+
+        }
+    }
+}
