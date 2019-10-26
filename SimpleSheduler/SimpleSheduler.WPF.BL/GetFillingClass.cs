d7e41ae9a008c81dce52789a0da329912f126887
dimaematveev <dimaematveev@gmail.com>
2019-10-26 17:33:14 +0300

    Добавил некоторые комментарии и Включение отключение кнопок!

--- SimpleSheduler/SimpleSheduler.WPF.BL/GetFillingClass.cs
+++ SimpleSheduler/SimpleSheduler.WPF.BL/GetFillingClass.cs
@@ -3,6 +3,7 @@ using SimpleSheduler.BD.Model;
 using SimpleSheduler.BL;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,19 @@ namespace SimpleSheduler.WPF.BL
 {
     public class GetFillingClass
     {
+        public List<Filling<Group>> fillingGroups;
+        public List<Filling<Classroom>> fillingClassrooms;
+
+
         /// <summary>
-        /// Получить заполнение по каждому (преподавателю,группе,аудитории)
+        /// Получить заполнение по каждому (группе,аудитории)
         /// </summary>
         /// <typeparam name="T">(преподавателю,группе,аудитории)</typeparam>
         /// <param name="array">массив (преподавателю,группе,аудитории)</param>
         /// <param name="pairs"> массив пар</param>
         /// <param name="studyDays">Массив учебных дней</param>
         /// <returns>массив заполнение по каждому (преподавателю,группе,аудитории)</returns>
-        public static List<Filling<T>> GetFilling<T>(List<T> array, List<Pair> pairs, List<StudyDay> studyDays) where T : class, IName
+        private List<Filling<T>> GetFilling<T>(List<T> array, List<Pair> pairs, List<StudyDay> studyDays) where T : class, IName
         {
             var result = new List<Filling<T>>();
             foreach (var item in array)
@@ -28,5 +33,193 @@ namespace SimpleSheduler.WPF.BL
             }
             return result;
         }
+
+        /// <summary>
+        /// Поличть все заполения
+        /// </summary>
+        /// <param name="getDataFromBD">БД</param>
+        public void GetFilling(GetDataFromBD getDataFromBD)
+        {
+            fillingGroups = GetFilling(getDataFromBD.groups, getDataFromBD.pairs, getDataFromBD.studyDays);
+            fillingClassrooms = GetFilling(getDataFromBD.classrooms, getDataFromBD.pairs, getDataFromBD.studyDays);
+        }
+
+
+        /// <summary>
+        /// Получить таблицу с Данными по заполнению
+        /// </summary>
+        /// <param name="sNamespace"> Какую таблицу получаем typeof(Class).FullName</param>
+        /// <param name="createScheduler"> Созданое рассписание</param>
+        /// <returns></returns>
+        public DataTable GetDateTableFilling(string sNamespace, CreateScheduler createScheduler)
+        {
+            if (sNamespace == typeof(Filling<Classroom>).FullName)
+            {
+                DataTable table = GetDateTableFilling(createScheduler.FillingClassrooms, false);
+                table.TableName = "Заполнение Аудиторий";
+                table.Namespace = typeof(Filling<Classroom>).FullName;
+                return table;
+
+            }
+            if (sNamespace == typeof(Filling<Group>).FullName)
+            {
+                DataTable table = GetDateTableFilling(createScheduler.FillingGroups, false);
+                table.TableName = "Заполнение Групп";
+                table.Namespace = typeof(Filling<Group>).FullName;
+                return table;
+            }
+            return null;
+        }
+        /// <summary>
+        /// Получит таблицу для заполнения.
+        /// </summary>
+        /// <param name="sNamespace"> Какую таблицу получаем typeof(Class).FullName</param>
+        /// <returns></returns>
+        public DataTable GetDateTableFilling(string sNamespace)
+        {
+            if (sNamespace==typeof(Filling<Classroom>).FullName)
+            {
+                DataTable table = GetDateTableFilling(fillingClassrooms, true);
+                table.TableName = "Заполнение Аудиторий";
+                table.Namespace = typeof(Filling<Classroom>).FullName;
+                return table;
+
+            }
+            if (sNamespace == typeof(Filling<Group>).FullName)
+            {
+                DataTable table = GetDateTableFilling(fillingGroups, true);
+                table.TableName = "Заполнение Групп";
+                table.Namespace = typeof(Filling<Group>).FullName;
+                return table;
+            }
+            return null;
+        }
+
+        private DataTable GetDateTableFilling<T>(List<Filling<T>> fillings, bool set = true) where T : class, IName
+        {
+            DataTable table = new DataTable();
+            {
+                DataColumn column;
+                column = new DataColumn
+                {
+                    DataType = typeof(int),
+                    ColumnName = "NumberOfWeek",
+                    Caption = "Номер недели"
+                };
+                // Add the Column to the DataColumnCollection.
+                table.Columns.Add(column);
+
+                column = new DataColumn
+                {
+                    DataType = typeof(string),
+                    ColumnName = "NameDayOfWeek",
+                    Caption = "День недели"
+                };
+                // Add the Column to the DataColumnCollection.
+                table.Columns.Add(column);
+
+                column = new DataColumn
+                {
+                    DataType = typeof(int),
+                    ColumnName = "NumberThePair",
+                    Caption = "Номер Пары"
+                };
+                // Add the Column to the DataColumnCollection.
+                table.Columns.Add(column);
+
+                foreach (var filling in fillings)
+                {
+                    if (set)
+                    {
+                        column = new DataColumn
+                        {
+
+                            DataType = typeof(string),
+                            ColumnName = $"{filling.Value.NameString()}",
+                            Caption = $"{filling.Value.NameString()}"
+                        };
+                    }
+                    else
+                    {
+                        column = new DataColumn
+                        {
+
+                            DataType = typeof(BusyPair),
+                            ColumnName = $"{filling.Value.NameString()}",
+                            Caption = $"{filling.Value.NameString()}"
+                        };
+                    }
+
+                    // Add the Column to the DataColumnCollection.
+                    table.Columns.Add(column);
+                }
+            }
+            for (int i = 0; i < fillings[0].PossibleFillings.Count; i++)
+            {
+                DataRow row;
+                row = table.NewRow();
+                var temp = fillings[0].PossibleFillings[i];
+                row[0] = $"{temp.StudyDay.NumberOfWeek}";
+                row[1] = $"{temp.StudyDay.NameDayOfWeek}";
+                row[2] = $"{temp.Pair.NumberThePair}";
+                int stolb = 3;
+                foreach (var filling in fillings)
+                {
+                    if (set)
+                    {
+                        string IsPair;
+                        var BusyPair = filling.PossibleFillings[i].BusyPair;
+                        IsPair = BusyPair == null ? null : BusyPair.Subject.Name;
+                        row[stolb] = IsPair;
+                    }
+                    else
+                    {
+                        row[stolb] = filling.PossibleFillings[i].BusyPair;
+                    }
+
+                    stolb++;
+                }
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+
+        public void SetDateTableFilling(DataTable dataTable)
+        {
+            if (dataTable.Namespace == typeof(Filling<Group>).FullName)
+            {
+                SetDateTableFilling(dataTable, fillingGroups);
+            }
+            if (dataTable.Namespace == typeof(Filling<Classroom>).FullName)
+            {
+                SetDateTableFilling(dataTable, fillingClassrooms);
+            }
+        }
+
+        private void SetDateTableFilling<T>(DataTable dataTable, List<Filling<T>> fillings) where T : class, IName
+        {
+
+            for (int i = 0; i < fillings.Count; i++)
+            {
+                for (int j = 0; j < dataTable.Rows.Count; j++)
+                {
+                    BusyPair busyPair;
+                    if (dataTable.Rows[j][3 + i].ToString() == "")
+                    {
+                        busyPair = null;
+                    }
+                    else
+                    {
+                        busyPair = new BusyPair(new Classroom(), new Subject() { Name = (string)dataTable.Rows[j][3 + i] }, new Group());
+                    }
+                    fillings[i].PossibleFillings[j].BusyPair = busyPair;
+                }
+
+            }
+
+        }
+
+
     }
 }
--- SimpleSheduler/SimpleSheduler.WPF/MainWindow.xaml.cs
+++ SimpleSheduler/SimpleSheduler.WPF/MainWindow.xaml.cs
@@ -30,49 +30,37 @@ namespace SimpleSheduler.WPF
 
 
         //Данные из БД
-        readonly GetDataFromBD getDataFromBD = new GetDataFromBD();
-        List<Filling<Group>> fillingGroups;
-        List<Filling<Classroom>> fillingClassrooms;
+        readonly private GetDataFromBD getDataFromBD = new GetDataFromBD();
+        //Данные по заполнению
+        private GetFillingClass getFillingClass;
+       
         CreateScheduler createScheduler1;
         public MainWindow()
         {
             GetDataFromBD.RepositoryBase();
             InitializeComponent();
             ButtonGetDataFromBD.Click += ButtonGetDataFromBD_Click;
-           
-            ButtonOpenClassroom.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(sender1, EventArgs1, typeof(Classroom).FullName); };
-            ButtonOpenSubject.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(sender1, EventArgs1, typeof(Subject).FullName); };
-            ButtonOpenCurricila.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(sender1, EventArgs1, typeof(Curriculum).FullName); };
-            ButtonOpenGroup.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(sender1, EventArgs1, typeof(Group).FullName); };
-            ButtonOpenPair.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(sender1, EventArgs1, typeof(Pair).FullName); };
-            ButtonOpenStudyDay.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(sender1, EventArgs1, typeof(StudyDay).FullName); };
+
+            ButtonOpenClassroom.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(typeof(Classroom).FullName); };
+            ButtonOpenSubject.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(typeof(Subject).FullName); };
+            ButtonOpenCurricila.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(typeof(Curriculum).FullName); };
+            ButtonOpenGroup.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(typeof(Group).FullName); };
+            ButtonOpenPair.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(typeof(Pair).FullName); };
+            ButtonOpenStudyDay.Click += (sender1, EventArgs1) => { ButtonOpenBD_Click(typeof(StudyDay).FullName); };
 
 
 
             GetFilling.Click += GetFilling_Click;
-            SetFillingClassrooms.Click += SetFillingClassrooms_Click;
-            SetFillingGroups.Click += SetFillingGroups_Click;
+            SetFillingClassrooms.Click += (sender1, EventArgs1) => { SetFilling_Click(typeof(Filling<Classroom>).FullName); };
+            SetFillingGroups.Click += (sender1, EventArgs1) => { SetFilling_Click(typeof(Filling<Group>).FullName); };
             CreateScheduler.Click += CreateScheduler_Click;
-
-            GetFillingClassrooms.Click += GetFillingClassrooms_Click; ;
-            GetFillingGroups.Click += GetFillingGroups_Click; ;
+            GetFillingClassrooms.Click += (sender1, EventArgs1) => { GetFillingFromScheduler_Click(typeof(Filling<Classroom>).FullName); };
+            GetFillingGroups.Click += (sender1, EventArgs1) => { GetFillingFromScheduler_Click(typeof(Filling<Group>).FullName); };
             this.Loaded += MainWindow_Loaded;
         }
-
-        private void GetFillingGroups_Click(object sender, RoutedEventArgs e)
-        {
-            var table = GetDateTableFilling(createScheduler1.FillingGroups, false);
-            table.TableName = "Заполнение Групп";
-            OpenGridBD(table);
-        }
-
-        private void GetFillingClassrooms_Click(object sender, RoutedEventArgs e)
-        {
-            var table = GetDateTableFilling(createScheduler1.FillingClassrooms, false);
-            table.TableName = "Заполнение Аудиторий";
-            OpenGridBD(table);
-        }
-
+        /// <summary>
+        /// Для упрощения процесса запущу сразу
+        /// </summary>
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             ButtonGetDataFromBD_Click(sender,e);
@@ -80,58 +68,34 @@ namespace SimpleSheduler.WPF
             CreateScheduler_Click(sender, e);
         }
 
-        private void CreateScheduler_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// В начале какие кнопки активны
+        /// </summary>
+        private void ButtonEnable_Start()
         {
-            createScheduler1 = new CreateScheduler(getDataFromBD.groups, getDataFromBD.classrooms, getDataFromBD.subjects, getDataFromBD.curricula, fillingGroups, fillingClassrooms);
-
-            GetFillingClassrooms.IsEnabled = true;
-            GetFillingGroups.IsEnabled = true;
+            ButtonOpenGroup.IsEnabled = false;
+            ButtonOpenClassroom.IsEnabled = false;
+            ButtonOpenSubject.IsEnabled = false;
+            ButtonOpenCurricila.IsEnabled = false;
+            GetFilling.IsEnabled = false;
+            SetFillingGroups.IsEnabled = false;
+            SetFillingClassrooms.IsEnabled = false;
+            ButtonOpenPair.IsEnabled = false;
+            ButtonOpenStudyDay.IsEnabled = false;
+            CreateScheduler.IsEnabled = false;
+            GetFillingGroups.IsEnabled = false;
+            GetFillingClassrooms.IsEnabled = false;
         }
 
-        private void SetFillingGroups_Click(object sender, RoutedEventArgs e)
-        {
-            var table = GetDateTableFilling(fillingGroups,true);
-            table.TableName = "Заполнение Групп";
-            table.Namespace = typeof(Filling<Group>).FullName;
-            var outClassList = OpenGridBD(table, true);
-            outClassList.ButtonSave.Click += (sender1, EventArgs1) => { ButtonSaveFilling_Click(sender1, EventArgs1, outClassList); };
-        }
-        private void SetFillingClassrooms_Click(object sender, RoutedEventArgs e)
-        {
-            var table = GetDateTableFilling(fillingClassrooms,true);
-            table.TableName = "Заполнение Аудиторий";
-            table.Namespace = typeof(Filling<Classroom>).FullName;
-            var outClassList = OpenGridBD(table, true);
-            outClassList.ButtonSave.Click += (sender1, EventArgs1) => { ButtonSaveFilling_Click(sender1, EventArgs1, outClassList); };
-
-        }
-        
-        private void GetFilling_Click(object sender, RoutedEventArgs e)
-        {
-            fillingGroups = GetFillingClass.GetFilling(getDataFromBD.groups, getDataFromBD.pairs, getDataFromBD.studyDays);
-            fillingClassrooms = GetFillingClass.GetFilling(getDataFromBD.classrooms, getDataFromBD.pairs, getDataFromBD.studyDays);
-            //TODO: Сделать так чтобы просто при передаче обновлялись поля в новосозданной в CreateScheduler
-           // Сдесь только 1 и 0 по возможности занятости аудитории!!!! и группы соответстве
-            SetFillingClassrooms.IsEnabled = true;
-            SetFillingGroups.IsEnabled = true;
-            CreateScheduler.IsEnabled = true;
-
-        }
-
-
-        private void ButtonOpenBD_Click(object sender, RoutedEventArgs e, string sNamespace)
-        {
-            var table = getDataFromBD.GetDateTableBD(sNamespace);
-            var outClassList = OpenGridBD(table, true);
-            outClassList.ButtonSave.Click += (sender1, EventArgs1) => { ButtonSaveBD_Click(sender1, EventArgs1, outClassList); };
-        }
-
-       
-
-
+        /// <summary>
+        /// Подключение к базе и считывание из нее информации. 
+        /// Также дает доступ к кнопкам показывающие БД.
+        /// Инициализация Заполнения
+        /// </summary>
         private void ButtonGetDataFromBD_Click(object sender, RoutedEventArgs e)
         {
             getDataFromBD.ReadDB();
+            getFillingClass = new GetFillingClass();
             string ret = "";
             ret += $"Количество:\n";
             ret += $"Аудиторий={getDataFromBD.classrooms.Count}, ";
@@ -141,6 +105,13 @@ namespace SimpleSheduler.WPF
             ret += $"Пар в день={getDataFromBD.pairs.Count}, ";
             ret += $"Учебных дней за 2 недели={getDataFromBD.studyDays.Count}.";
             TexboxFromBD.Text = ret;
+
+            ButtonEnable_ButtonGetDataFromBD_Click();
+        }
+
+        private void ButtonEnable_ButtonGetDataFromBD_Click()
+        {
+            ButtonEnable_Start();
 
             if (getDataFromBD.groups != null)
             {
@@ -166,14 +137,31 @@ namespace SimpleSheduler.WPF
             {
                 ButtonOpenStudyDay.IsEnabled = true;
             }
-
-            if (getDataFromBD.groups != null && getDataFromBD.classrooms != null &&  getDataFromBD.pairs != null && getDataFromBD.studyDays != null)
+            if (getDataFromBD.groups != null && getDataFromBD.classrooms != null && getDataFromBD.pairs != null && getDataFromBD.studyDays != null)
             {
                 GetFilling.IsEnabled = true;
             }
         }
-       
-        
+        /// <summary>
+        /// Открытие для изменения Таблиц из БД. 
+        /// </summary>
+        /// <param name="sNamespace"> typeof(Class).FullName Полное имя класса который будем показывать </param>
+        private void ButtonOpenBD_Click( string sNamespace)
+        {
+            var table = getDataFromBD.GetDateTableBD(sNamespace);
+            
+
+            var outClassList = OpenGridBD(table, true);
+            //Кнопке save приписываем действие
+            outClassList.ButtonSave.Click += (sender1, EventArgs1) => { ButtonSaveBD_Click(outClassList); };
+        }
+
+        /// <summary>
+        /// Показывает DataTable  в новом окне 
+        /// </summary>
+        /// <param name="collection">Таблица которую стоит показать</param>
+        /// <param name="save">Активна ли кнопка сохранить. False - не активна, True- активна</param>
+        /// <returns> Ссылка на открытое окно</returns>
         private OutClassList OpenGridBD(DataTable collection, bool save = false)
         {
             OutClassList outGroup = new OutClassList();
@@ -183,142 +171,86 @@ namespace SimpleSheduler.WPF
             return outGroup;
         }
 
-        private void ButtonSaveBD_Click(object sender, RoutedEventArgs e, OutClassList outClassList)
+        /// <summary>
+        /// Действие кнопки Save в открытом окне
+        /// </summary>
+        /// <param name="outClassList">Ссылка на окно</param>
+        private void ButtonSaveBD_Click(OutClassList outClassList)
         {
             getDataFromBD.SetBD(outClassList.DataTable);
+
+        }
+
+        /// <summary>
+        /// Инициализировать GetFillingClass из БД. 
+        /// Дать возможность посмотреть и отредактировать Заполнение.
+        /// Дать возможность создать расписание.
+        /// </summary>
+        private void GetFilling_Click(object sender, RoutedEventArgs e)
+        {
+            getFillingClass.GetFilling(getDataFromBD);
+            ButtonEnable_GetFilling_Click();
+
+        }
+        private void ButtonEnable_GetFilling_Click()
+        {
+            ButtonEnable_ButtonGetDataFromBD_Click();
+            SetFillingClassrooms.IsEnabled = true;
+            SetFillingGroups.IsEnabled = true;
+            CreateScheduler.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Возможность изменить заполнение аудиторий
+        /// </summary>
+        private void SetFilling_Click(string sNamespace)
+        {
+            var table = getFillingClass.GetDateTableFilling(sNamespace);
             
+            var outClassList = OpenGridBD(table, true);
+            outClassList.ButtonSave.Click += (sender1, EventArgs1) => { ButtonSaveFilling_Click(outClassList); };
+
         }
 
-        private void ButtonSaveFilling_Click(object sender, RoutedEventArgs e, OutClassList outClassList)
+        /// <summary>
+        ///  Действие кнопки Save в открытом окне Заполнения
+        /// </summary>
+        /// <param name="outClassList">Окно</param>
+        private void ButtonSaveFilling_Click(OutClassList outClassList)
         {
-            SetDateTableFilling(outClassList.DataTable);
+            getFillingClass.SetDateTableFilling(outClassList.DataTable);
 
 
         }
 
-        private DataTable GetDateTableFilling<T>(List<Filling<T>> fillings , bool set = true) where T : class, IName
+        /// <summary>
+        /// Создание рассписания на основе данных из БД и заполнения.
+        /// Дает возможеость посмотреть составленное расписание.
+        /// </summary>
+        private void CreateScheduler_Click(object sender, RoutedEventArgs e)
         {
-            DataTable table = new DataTable();
-            {
-                DataColumn column;
-                column = new DataColumn
-                {
-                    DataType = typeof(int),
-                    ColumnName = "NumberOfWeek",
-                    Caption = "Номер недели"
-                };
-                // Add the Column to the DataColumnCollection.
-                table.Columns.Add(column);
-
-                column = new DataColumn
-                {
-                    DataType = typeof(string),
-                    ColumnName = "NameDayOfWeek",
-                    Caption = "День недели"
-                };
-                // Add the Column to the DataColumnCollection.
-                table.Columns.Add(column);
-
-                column = new DataColumn
-                {
-                    DataType = typeof(int),
-                    ColumnName = "NumberThePair",
-                    Caption = "Номер Пары"
-                };
-                // Add the Column to the DataColumnCollection.
-                table.Columns.Add(column);
-
-                foreach (var filling in fillings)
-                {
-                    if (set)
-                    {
-                        column = new DataColumn
-                        {
-
-                            DataType = typeof(string),
-                            ColumnName = $"{filling.Value.NameString()}",
-                            Caption = $"{filling.Value.NameString()}"
-                        };
-                    }
-                    else
-                    {
-                        column = new DataColumn
-                        {
-
-                            DataType = typeof(BusyPair),
-                            ColumnName = $"{filling.Value.NameString()}",
-                            Caption = $"{filling.Value.NameString()}"
-                        };
-                    }
-                    
-                    // Add the Column to the DataColumnCollection.
-                    table.Columns.Add(column);
-                }
-            }
-            for (int i = 0; i < fillings[0].PossibleFillings.Count; i++)
-            {
-                DataRow row;
-                row = table.NewRow();
-                var temp = fillings[0].PossibleFillings[i];
-                row[0] = $"{temp.StudyDay.NumberOfWeek}";
-                row[1] = $"{temp.StudyDay.NameDayOfWeek}";
-                row[2] = $"{temp.Pair.NumberThePair}";
-                int stolb = 3;
-                foreach (var filling in fillings)
-                {
-                    if (set)
-                    {
-                        string IsPair;
-                        var BusyPair = filling.PossibleFillings[i].BusyPair;
-                        IsPair = BusyPair == null ? null : BusyPair.Subject.Name;
-                        row[stolb] = IsPair;
-                    }
-                    else
-                    {
-                        row[stolb] = filling.PossibleFillings[i].BusyPair;
-                    }
-                       
-                    stolb++;
-                }
-                table.Rows.Add(row);
-            }
-            return table;
+            createScheduler1 = new CreateScheduler(getDataFromBD.groups, getDataFromBD.classrooms, getDataFromBD.subjects, getDataFromBD.curricula, getFillingClass.fillingGroups, getFillingClass.fillingClassrooms);
+            ButtonEnable_CreateScheduler_Click();
         }
-
-
-        private void SetDateTableFilling(DataTable dataTable)
+        private void ButtonEnable_CreateScheduler_Click()
         {
-            if (dataTable.Namespace == typeof(Filling<Group>).FullName)
-            {
-                SetDateTableFilling(dataTable, fillingGroups);
-            }
-            if (dataTable.Namespace == typeof(Filling<Classroom>).FullName)
-            {
-                SetDateTableFilling(dataTable, fillingClassrooms);
-            }
+            ButtonEnable_GetFilling_Click();
+            GetFillingClassrooms.IsEnabled = true;
+            GetFillingGroups.IsEnabled = true;
         }
 
-        private void SetDateTableFilling<T>(DataTable dataTable, List<Filling<T>> fillings) where T:class,IName
+
+        /// <summary>
+        /// Возможность посмотреть заполнение аудиторий в созданном рассписании
+        /// </summary>
+        private void GetFillingFromScheduler_Click(string sNamespace)
         {
-            
-            for (int i = 0; i < fillings.Count; i++)
-            {
-                for (int j = 0; j < dataTable.Rows.Count; j++)
-                {
-                    BusyPair busyPair;
-                    if (dataTable.Rows[j][3 + i].ToString() == "")
-                    {
-                        busyPair = null;
-                    }
-                    else
-                    {
-                        busyPair = new BusyPair(new Classroom(), new Subject() { Name = (string)dataTable.Rows[j][3 + i] }, new Group());
-                    }
-                    fillings[i].PossibleFillings[j].BusyPair = busyPair;
-                }
-               
-            }
-            
+            var table = getFillingClass.GetDateTableFilling(sNamespace, createScheduler1);
+
+            var outClassList = OpenGridBD(table, false);
         }
+
+
+
     }
 }
