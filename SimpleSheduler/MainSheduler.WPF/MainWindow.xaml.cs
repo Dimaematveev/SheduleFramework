4b5ebdff07089fa076878cd619c2b6708be7d1d0
dimaematveev <dimaematveev@gmail.com>
2020-03-01 17:49:45 +0300

    Создал классы для дней

--- SimpleSheduler/MainSheduler.WPF/MainWindow.xaml.cs
+++ SimpleSheduler/MainSheduler.WPF/MainWindow.xaml.cs
@@ -24,9 +24,7 @@ namespace MainSheduler.WPF
         private DateTime? Begin = null;
         private DateTime? End = null;
         private HelperDate helperDate;
-        private SortedDictionary<DateTime, string> ReducedDay = new SortedDictionary<DateTime, string>();
-        private SortedDictionary<DateTime, string> DayOff = new SortedDictionary<DateTime, string>();
-        private SortedDictionary<DateTime, string> WorkDay = new SortedDictionary<DateTime, string>();
+        PairsToDays pairsToDays;
 
         
         public MainWindow()
@@ -41,9 +39,7 @@ namespace MainSheduler.WPF
             DatePickerEnd.SelectedDateChanged += DatePickerEnd_SelectedDateChanged;
             ButtonOK.Click += ButtonOK_Click;
 
-            ListViewReducedDay.ItemsSource = ReducedDay.Values;
-            ListViewDayOff.ItemsSource = DayOff.Values;
-            ListViewWorkDay.ItemsSource = WorkDay.Values;
+           
         }
 
         private void DatePickerBegin_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
@@ -84,10 +80,10 @@ namespace MainSheduler.WPF
             ContextMenu contextMenu = new ContextMenu();
             MenuItem menuItemWorkDay = new MenuItem();
             menuItemWorkDay.Header = "Рабочие дни";
-            menuItemWorkDay.Click += MenuItemWorkDay_Click;
+            //menuItemWorkDay.Click += MenuItemWorkDay_Click;
             MenuItem menuItemDayOff = new MenuItem();
             menuItemDayOff.Header = "Выходные дни";
-            menuItemDayOff.Click += MenuItemDayOff_Click;
+            //menuItemDayOff.Click += MenuItemDayOff_Click;
             MenuItem menuItemReducedDay = new MenuItem();
             menuItemReducedDay.Header = "Сокращенные дни";
             for (int i = 1; i <= 6; i++)
@@ -95,7 +91,7 @@ namespace MainSheduler.WPF
                 int z = i;
                 MenuItem menuItemReducedDayCountPair = new MenuItem();
                 menuItemReducedDayCountPair.Header = $"Кол-во пар: {z}";
-                menuItemReducedDayCountPair.Click += (sender1, EventArgs1) => { MenuItemReducedDayCountPair_Click(z); }; 
+                //menuItemReducedDayCountPair.Click += (sender1, EventArgs1) => { MenuItemReducedDayCountPair_Click(z); }; 
                 menuItemReducedDay.Items.Add(menuItemReducedDayCountPair);
             }
             contextMenu.Items.Add(menuItemWorkDay);
@@ -103,74 +99,45 @@ namespace MainSheduler.WPF
             contextMenu.Items.Add(menuItemReducedDay);
             Calendar1.ContextMenu = contextMenu;
 
-            //var aa = Calendar1.Template;
-            //Calendar1.BorderBrush.Opacity = 1;
-            //ControlTemplate controlTemplate = new ControlTemplate(typeof(System.Windows.Controls.Calendar));
-            ////controlTemplate.Triggers.Add((new Trigger()).);
-            ////controlTemplate.
-            //var w = 1;\
-
-            for (DateTime i = Begin.Value; i <= End.Value; i=i.AddDays(1))
-            {
-                if (i.DayOfWeek == DayOfWeek.Sunday) 
-                {
-                    AddCollection(DayOff, i);
-                }
-                else
-                {
-                    AddCollection(WorkDay, i);
-                }
-            }
+            PairsToDays pairsToDays = new PairsToDays(Begin.Value, End.Value, 6);
+            ListViewReducedDay.ItemsSource = pairsToDays.GetReducedDay();
+            ListViewDayOff.ItemsSource = pairsToDays.GetDayOff();
+            ListViewWorkDay.ItemsSource = pairsToDays.GetWorkDay();
             UpdateAllListView();
         }
 
-        private void AddCollection(SortedDictionary<DateTime, string> collection, DateTime dateTime, int? count = null)
-        {
-            if (!collection.Keys.Contains(dateTime))
-            {
-                if (count == null)
-                {
-                    collection.Add(dateTime, $"[{dateTime.ToShortDateString()}]");
-                }
-                else
-                {
-                    collection.Add(dateTime, $"[{dateTime.ToShortDateString()},{count}]");
-                }
-            }
-            
-        }
-        private void MenuItemReducedDayCountPair_Click(int count)
-        {
-            foreach (var item in Calendar1.SelectedDates)
-            {
-                WorkDay.Remove(item);
-                AddCollection(ReducedDay, item, count);
-                DayOff.Remove(item);
-            }
-            UpdateAllListView();
-        }
+        //private void MenuItemReducedDayCountPair_Click(int count)
+        //{
+        //    foreach (var item in Calendar1.SelectedDates)
+        //    {
+        //        WorkDay.Remove(item);
+        //        AddCollection(ReducedDay, item, count);
+        //        DayOff.Remove(item);
+        //    }
+        //    UpdateAllListView();
+        //}
 
-        private void MenuItemDayOff_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (var item in Calendar1.SelectedDates)
-            {
-                WorkDay.Remove(item);
-                ReducedDay.Remove(item);
-                AddCollection(DayOff, item);
-            }
-            UpdateAllListView();
-        }
+        //private void MenuItemDayOff_Click(object sender, RoutedEventArgs e)
+        //{
+        //    foreach (var item in Calendar1.SelectedDates)
+        //    {
+        //        WorkDay.Remove(item);
+        //        ReducedDay.Remove(item);
+        //        AddCollection(DayOff, item);
+        //    }
+        //    UpdateAllListView();
+        //}
 
-        private void MenuItemWorkDay_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (var item in Calendar1.SelectedDates)
-            {
-                AddCollection(WorkDay, item);
-                ReducedDay.Remove(item);
-                DayOff.Remove(item);
-            }
-            UpdateAllListView();
-        }
+        //private void MenuItemWorkDay_Click(object sender, RoutedEventArgs e)
+        //{
+        //    foreach (var item in Calendar1.SelectedDates)
+        //    {
+        //        AddCollection(WorkDay, item);
+        //        ReducedDay.Remove(item);
+        //        DayOff.Remove(item);
+        //    }
+        //    UpdateAllListView();
+        //}
 
         private void UpdateAllListView()
         {
---
+++ SimpleSheduler/MainSheduler.WPF/PairsToDay.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainSheduler.WPF
+{
+    /// <summary>
+    /// Класс количество пар по дням.
+    /// </summary>
+    public class PairsToDay: IComparable
+    {
+        
+        /// <summary>
+        /// Дата пары
+        /// </summary>
+        public DateTime DatePair { get; set; }
+        /// <summary>
+        /// Количество пар если null значит полный день, если 0 то выходной, иначе день сокращен до стольких пар
+        /// </summary>
+        public int? CountPair { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="datePair">Дата пары</param>
+        /// <param name="countPair">Количество пар если null значит полный день, если 0 то выходной, иначе день сокращен до стольких пар</param>
+        public PairsToDay(DateTime datePair, int? countPair)
+        {
+            DatePair = datePair;
+            CountPair = countPair;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            if (CountPair > 0)
+            {
+                str += $"[{DatePair.ToShortDateString()},{CountPair}]";
+            }
+            else
+            {
+                str += $"[{DatePair.ToShortDateString()}]";
+            }
+            return str;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return DatePair.Equals(((PairsToDay)obj).DatePair);
+        }
+
+        public int CompareTo(object obj)
+        {
+            return DatePair.CompareTo(((PairsToDay)obj).DatePair);
+        }
+    }
+
+    /// <summary>
+    /// пары по всем дням
+    /// </summary>
+    public class PairsToDays
+    {
+        
+
+        /// <summary>
+        /// Список количества пар для всех дней
+        /// </summary>
+        public List<PairsToDay> pairsToDays { get; set; }
+
+        /// <summary>
+        /// Максимальное кол-во пар
+        /// </summary>
+        public int MaxCountPair { get; }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pairsToDayList">Список всех кол-во пар по дням</param>
+        /// <param name="maxCountPair"> максимальное кол-во пар в день</param>
+        public PairsToDays(IEnumerable<PairsToDay> pairsToDayList, int maxCountPair)
+        {
+            pairsToDays = pairsToDayList.ToList();
+            MaxCountPair = maxCountPair;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="beginDate">Дата начала отсчета пар</param>
+        /// <param name="endDay">Дата окончания отсчета пар</param>
+        /// <param name="maxCountPair"> максимальное кол-во пар в день</param>
+        public PairsToDays(DateTime beginDate, DateTime endDay, int maxCountPair)
+        {
+            var PairsToDayList = new List<PairsToDay>();
+
+            for (DateTime i = beginDate; i <= endDay; i = i.AddDays(1))
+            {
+                if (i.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    PairsToDayList.Add(new PairsToDay(i, 0));
+                }
+                else
+                {
+                    PairsToDayList.Add(new PairsToDay(i, null));
+                }
+            }
+            pairsToDays = PairsToDayList;
+            MaxCountPair = maxCountPair;
+        }
+
+        /// <summary>
+        /// Получить все полные рабочие дни
+        /// </summary>
+        /// <returns> список всех рабочих дней</returns>
+        public List<PairsToDay> GetWorkDay()
+        {
+            var ret =  pairsToDays.Where(x => x.CountPair == null).ToList();
+            ret.Sort();
+            return ret;
+        }
+        /// <summary>
+        /// Получить все выходные
+        /// </summary>
+        /// <returns> список всех выходных</returns>
+        public List<PairsToDay> GetDayOff()
+        {
+            var ret = pairsToDays.Where(x => x.CountPair == 0).ToList();
+            ret.Sort();
+            return ret;
+        }
+        /// <summary>
+        /// Получить все неполные рабочие дни
+        /// </summary>
+        /// <returns> список всех неполных рабочих дней</returns>
+        public List<PairsToDay> GetReducedDay()
+        {
+            var ret = pairsToDays.Where(x => x.CountPair > 0).ToList();
+            ret.Sort();
+            return ret;
+        }
+    }
+}
