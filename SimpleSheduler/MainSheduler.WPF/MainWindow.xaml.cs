2fbcb5db09113563babe4db702eba6ce0e8050ae
dimaematveev <dimaematveev@gmail.com>
2020-03-01 15:06:36 +0300

    Получил сортированные списки

--- SimpleSheduler/MainSheduler.WPF/MainWindow.xaml.cs
+++ SimpleSheduler/MainSheduler.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -20,11 +21,17 @@ namespace MainSheduler.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        DateTime? Begin = null;
-        DateTime? End = null;
-        HelperDate helperDate;
+        private DateTime? Begin = null;
+        private DateTime? End = null;
+        private HelperDate helperDate;
+        private SortedDictionary<DateTime, int> ReducedDay = new SortedDictionary<DateTime, int>();
+        private SortedSet<DateTime> DayOff = new SortedSet<DateTime>();
+        private SortedSet<DateTime> WorkDay = new SortedSet<DateTime>();
+
+        
         public MainWindow()
         {
+            
             InitializeComponent();
             Begin = new DateTime(2020, 2, 01);
             End = new DateTime(2020, 2, 28);
@@ -35,7 +42,11 @@ namespace MainSheduler.WPF
             DatePickerEnd.SelectedDateChanged += DatePickerEnd_SelectedDateChanged;
             ButtonOK.Click += ButtonOK_Click;
 
-                       
+
+
+            ListViewReducedDay.ItemsSource = ReducedDay;
+            ListViewDayOff.ItemsSource = DayOff;
+            ListViewWorkDay.ItemsSource = WorkDay;
         }
 
         private void DatePickerBegin_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
@@ -70,34 +81,84 @@ namespace MainSheduler.WPF
             Calendar1.DisplayDateStart = Begin;
             Calendar1.DisplayDateEnd = End;
             Calendar1.SelectionMode = CalendarSelectionMode.MultipleRange;
-            Calendar1.MouseRightButtonUp += Calendar1_MouseRightButtonUp;
+        
 
 
             ContextMenu contextMenu = new ContextMenu();
             MenuItem menuItemWorkDay = new MenuItem();
             menuItemWorkDay.Header = "Рабочие дни";
+            menuItemWorkDay.Click += MenuItemWorkDay_Click;
             MenuItem menuItemDayOff = new MenuItem();
             menuItemDayOff.Header = "Выходные дни";
+            menuItemDayOff.Click += MenuItemDayOff_Click;
             MenuItem menuItemReducedDay = new MenuItem();
             menuItemReducedDay.Header = "Сокращенные дни";
             for (int i = 1; i <= 6; i++)
             {
+                int z = i;
                 MenuItem menuItemReducedDayCountPair = new MenuItem();
-                menuItemReducedDayCountPair.Header = $"{i}";
+                menuItemReducedDayCountPair.Header = $"Кол-во пар: {z}";
+                menuItemReducedDayCountPair.Click += (sender1, EventArgs1) => { MenuItemReducedDayCountPair_Click(z); }; 
                 menuItemReducedDay.Items.Add(menuItemReducedDayCountPair);
             }
             contextMenu.Items.Add(menuItemWorkDay);
             contextMenu.Items.Add(menuItemDayOff);
             contextMenu.Items.Add(menuItemReducedDay);
             Calendar1.ContextMenu = contextMenu;
-            Calendar1.SelectedDates.
+
+            //var aa = Calendar1.Template;
+            //Calendar1.BorderBrush.Opacity = 1;
+            //ControlTemplate controlTemplate = new ControlTemplate(typeof(System.Windows.Controls.Calendar));
+            ////controlTemplate.Triggers.Add((new Trigger()).);
+            ////controlTemplate.
+            //var w = 1;
         }
 
-        private void Calendar1_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+
+        private void MenuItemReducedDayCountPair_Click(int count)
         {
-            
-            
+            foreach (var item in Calendar1.SelectedDates)
+            {
+                WorkDay.Remove(item);
+                ReducedDay.Add(item, count);
+                DayOff.Remove(item);
+            }
+            UpdateAllListView();
         }
+
+        private void MenuItemDayOff_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in Calendar1.SelectedDates)
+            {
+                WorkDay.Remove(item);
+                ReducedDay.Remove(item);
+                DayOff.Add(item);
+            }
+
+            UpdateAllListView();
+        }
+
+        private void MenuItemWorkDay_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in Calendar1.SelectedDates)
+            {
+                WorkDay.Add(item);
+                ReducedDay.Remove(item);
+                DayOff.Remove(item);
+            }
+            UpdateAllListView();
+        }
+
+        private void UpdateAllListView()
+        {
+            ListViewWorkDay.BeginInit();
+            ListViewWorkDay.EndInit();
+            ListViewReducedDay.BeginInit();
+            ListViewReducedDay.EndInit();
+            ListViewDayOff.BeginInit();
+            ListViewDayOff.EndInit();
+        }
+
     }
     
 }
