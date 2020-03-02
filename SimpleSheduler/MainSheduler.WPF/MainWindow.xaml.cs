e1c01791b4a22cfed0681410e60ae291e3e51e8f
dimaematveev <dimaematveev@gmail.com>
2020-03-02 17:26:30 +0300

    Кое что придумал насчет переноса пары с .... на выходной. Потом надо подумать неад реализациеЭ.э также сделал список выходных ней 2020 г.

--- SimpleSheduler/MainSheduler.WPF/MainWindow.xaml.cs
+++ SimpleSheduler/MainSheduler.WPF/MainWindow.xaml.cs
@@ -23,18 +23,60 @@ namespace MainSheduler.WPF
     {
         private HelperDate helperDate;
         PairsToDays pairsToDays;
-
+        /// <summary>
+        /// Праздничные дни
+        /// </summary>
+        List<DateTime> Dayoff1 = new List<DateTime>
+        {
+            new DateTime(2020,1,1),
+            new DateTime(2020,1,2),
+            new DateTime(2020,1,3),
+            new DateTime(2020,1,4),
+            new DateTime(2020,1,5),
+            new DateTime(2020,1,6),
+            new DateTime(2020,1,7),
+            new DateTime(2020,1,8),
+            new DateTime(2020,1,9),
+            new DateTime(2020,2,22),
+            new DateTime(2020,2,23),
+            new DateTime(2020,2,24),
+            new DateTime(2020,3,7),
+            new DateTime(2020,3,8),
+            new DateTime(2020,3,9),
+            new DateTime(2020,5,1),
+            new DateTime(2020,5,2),
+            new DateTime(2020,5,3),
+            new DateTime(2020,5,4),
+            new DateTime(2020,5,8),
+            new DateTime(2020,5,9),
+            new DateTime(2020,5,10),
+            new DateTime(2020,6,12),
+            new DateTime(2020,6,13),
+        };
         
         public MainWindow()
         {
             InitializeComponent();
-            DatePickerBegin.SelectedDate = new DateTime(2020, 2, 01);
-            DatePickerEnd.SelectedDate = new DateTime(2020, 2, 28);
-            ButtonOK.Click += ButtonOK_Click;
-            ButtonProv.Click += ButtonProv_Click;
+            DatePickerBegin.SelectedDate = new DateTime(2020, 1, 01);
+            DatePickerEnd.SelectedDate = new DateTime(2020, 6, 28);
+            ButtonOK.Click += (sender1, EventArgs1) => { ButtonOK_Click(); };
+            ButtonProv.Click += (sender1, EventArgs1) => { ButtonProv_Click(); };
+
+            Loaded += (object sender, RoutedEventArgs e) => { MainWindow_Loaded(); };
         }
 
-        private void ButtonProv_Click(object sender, RoutedEventArgs e)
+        private void MainWindow_Loaded()
+        {
+            ButtonOK_Click();
+            
+
+            pairsToDays.ResetDays(Dayoff1, 0);
+            UpdateAllListView();
+
+            ButtonProv_Click();
+        }
+
+        private void ButtonProv_Click()
         {
             var DateRange = new List<DateTime>();
             DateRange.AddRange(pairsToDays.ReducedDay.Select(x => x.DatePair));
@@ -43,7 +85,7 @@ namespace MainSheduler.WPF
             TextBoxInfo.Text = helperDate.ToString();
         }
 
-        private void ButtonOK_Click(object sender, RoutedEventArgs e)
+        private void ButtonOK_Click()
         {
 
             helperDate = new HelperDate(DatePickerBegin.SelectedDate.Value, DatePickerEnd.SelectedDate.Value);
@@ -59,9 +101,11 @@ namespace MainSheduler.WPF
             MenuItem menuItemWorkDay = new MenuItem();
             menuItemWorkDay.Header = "Рабочие дни";
             menuItemWorkDay.Click += (sender1, EventArgs1) => { MenuItemDayCountPair_Click(null); };
+            contextMenu.Items.Add(menuItemWorkDay);
             MenuItem menuItemDayOff = new MenuItem();
             menuItemDayOff.Header = "Выходные дни";
             menuItemDayOff.Click += (sender1, EventArgs1) => { MenuItemDayCountPair_Click(0); };
+            contextMenu.Items.Add(menuItemDayOff);
             MenuItem menuItemReducedDay = new MenuItem();
             menuItemReducedDay.Header = "Сокращенные дни";
             for (int i = 1; i <= 6; i++)
@@ -72,9 +116,11 @@ namespace MainSheduler.WPF
                 menuItemReducedDayCountPair.Click += (sender1, EventArgs1) => { MenuItemDayCountPair_Click(z); };
                 menuItemReducedDay.Items.Add(menuItemReducedDayCountPair);
             }
-            contextMenu.Items.Add(menuItemWorkDay);
-            contextMenu.Items.Add(menuItemDayOff);
             contextMenu.Items.Add(menuItemReducedDay);
+            //TODO: это перенос дня, не сделано. после надо подумать над реализацией
+            MenuItem menuItemPerenos = new MenuItem();
+            menuItemDayOff.Header = "Перенос дня";
+            contextMenu.Items.Add(menuItemPerenos);
             Calendar1.ContextMenu = contextMenu;
 
             pairsToDays = new PairsToDays(DatePickerBegin.SelectedDate.Value, DatePickerEnd.SelectedDate.Value, 6);
