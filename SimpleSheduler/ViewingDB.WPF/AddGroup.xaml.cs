b11e9c8173696161968f2810c2dc1f8918f66fdb
dimaematveev <dimaematveev@gmail.com>
2020-04-04 05:38:39 +0300

    Добавление групп

---
+++ SimpleSheduler/ViewingDB.WPF/AddGroup.xaml.cs
@@ -0,0 +1,91 @@
+﻿using SimpleSheduler.BD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ViewingDB.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для AddGroup.xaml
+    /// </summary>
+    public partial class AddGroup : Window
+    {
+        public int ID { get; private set; }
+        public AddGroup(string nameGroup, string potok, string seminar)
+        {
+            InitializeComponent();
+            GroupName.Text = nameGroup;
+            SokrGroupName.Text = nameGroup;
+            Potok.Text = potok;
+            Seminar.Text = seminar;
+            ScrollBarNumberPeople.ValueChanged += ScrollBarNumberPeople_ValueChanged;
+            Add.Click += (sender, e) => { GroupAdd(); };
+        }
+
+        private void ScrollBarNumberPeople_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            NumberPeople.Text = ScrollBarNumberPeople.Value.ToString();
+        }
+
+        private void GroupAdd()
+        {
+            string notWork = "";
+            if (GroupName.Text == null || GroupName.Text == "") 
+            {
+                notWork += $"{nameof(GroupName)} is null\n";
+            }
+            if (SokrGroupName.Text == null || SokrGroupName.Text == "")
+            {
+                notWork += $"{nameof(SokrGroupName)} is null\n";
+            }
+            if (Potok.Text == null || Potok.Text == "")
+            {
+                notWork += $"{nameof(Potok)} is null\n";
+            }
+            if (Seminar.Text == null || Seminar.Text == "")
+            {
+                notWork += $"{nameof(Seminar)} is null\n";
+            }
+            if (NumberPeople.Text == null || NumberPeople.Text == "")
+            {
+                notWork += $"{nameof(NumberPeople)} is null\n";
+            }
+
+
+            if (notWork == "")
+            {
+                MessageBox.Show("Work");
+                var group = new Group()
+                {
+                    FullName = GroupName.Text,
+                    Abbreviation = SokrGroupName.Text,
+                    Potok = Potok.Text,
+                    Seminar = Seminar.Text,
+                    NumberOfPersons = Convert.ToInt32(NumberPeople.Text),
+                    GroupId = WorkToMyDbContext.groups.Max(x => x.GroupId) + 1
+                };
+                WorkToMyDbContext.groups.Add(group);
+                WorkToMyDbContext.SaveDB();
+                ID = group.GroupId;
+            }
+            else
+            {
+                notWork = "Не добавится группа пока: \n" + notWork;
+                MessageBox.Show(notWork);
+            }
+
+
+           
+        }
+    }
+}
--- SimpleSheduler/ViewingDB.WPF/Excel.xaml.cs
+++ SimpleSheduler/ViewingDB.WPF/Excel.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SimpleSheduler.BD;
 
 namespace ViewingDB.WPF
 {
@@ -23,14 +24,66 @@ namespace ViewingDB.WPF
         public Excel(string filename)
         {
             InitializeComponent();
-
+            Add.Click += (sender,e) =>{ Add_Click(); };
             Init(filename);
+        }
+
+        private void Add_Click()
+        {
+            var group = WorkToMyDbContext.groups.Find(x => x.FullName == GroupName.Text);
+            int groupID;
+            if (group == null) 
+            {
+                AddGroup addGroup = new AddGroup(GroupName.Text, Potok.Text, Seminar.Text);
+                addGroup.ShowDialog();
+                groupID = addGroup.ID;
+            }
+            else
+            {
+                MessageBox.Show($"ok {GroupName.Text} и {group.ToString()}");
+                groupID = group.GroupId;
+            }
+
+            var tabItem = (TabItem)ExcelControl.SelectedItem;
+
+            var dataGrid = (DataGrid)tabItem.Content;
+            string str = "";
+            foreach (var item in dataGrid.Items)
+            {
+                if (item as System.Data.DataRowView != null)
+                {
+                    var rows = (System.Data.DataRowView)item;
+                    var subject = WorkToMyDbContext.subjects.Find(x => x.FullName == rows.Row[2].Text);
+                    int subjectID;
+                    if (subject == null)
+                    {
+                        AddGroup addGroup = new AddGroup(GroupName.Text, Potok.Text, Seminar.Text);
+                        addGroup.ShowDialog();
+                        groupID = addGroup.ID;
+                    }
+                    else
+                    {
+                        MessageBox.Show($"ok {GroupName.Text} и {group.ToString()}");
+                        groupID = group.GroupId;
+                    }
+
+
+                    for (int i = 0; i < dataGrid.Columns.Count; i++)
+                    {
+                        str += rows.Row[i] + "__";
+                    }
+                    str += "\n";
+                }
+            }
+
+
+            MessageBox.Show($"{str}");
         }
 
         private void Init(string filename)
         {
-            EasyExcel easyExcel17 = new EasyExcel(filename);
-            foreach (var Course in easyExcel17.AllCourses)
+            EasyExcel easyExcel = new EasyExcel(filename);
+            foreach (var Course in easyExcel.AllCourses)
             {
                 DataGrid dataGrid1 = new DataGrid();
                 dataGrid1.ItemsSource = Course.Semester1.DefaultView;
@@ -48,12 +101,16 @@ namespace ViewingDB.WPF
             }
 
 
-            Year.Text = easyExcel17.Title.YearBegin;
-            Kafedra.Text = easyExcel17.Title.Kafedra;
-            Napravl.Text = easyExcel17.Title.Napravl;
-            Potok.Text = easyExcel17.Title.Potok;
-            Profil.Text = easyExcel17.Title.Profil;
+            FileName.Text = filename.Split('\\').Last();
+            GroupName.Text = filename.Split('_')[1];
+            Year.Text = easyExcel.Title.YearBegin;
+            Kafedra.Text = easyExcel.Title.Kafedra;
+            Napravl.Text = easyExcel.Title.Napravl;
+            Potok.Text = easyExcel.Title.Potok;
+            Profil.Text = easyExcel.Title.Profil;
 
+
+            
         }
     }
 }
